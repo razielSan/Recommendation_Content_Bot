64ea3c31bd4f363d5a66fd9ae8bd87f2a507eeb7
raziel <tvindzon@gmail.com>
2025-10-03 12:05:25 +0700

    update app.py (create function on_startup, update main)

--- app/app.py
+++ app/app.py
@@ -1,13 +1,18 @@
 import asyncio
 
 from extensions import bot, dp
+from config.settings import settings
 
 
-async def on_statup():
+async def on_statup() -> None:
+    """Функция срабатывает при старте бота."""
     print("Бот запущенн")
 
 
-async def main():
+async def main() -> None:
+    """Главная функция."""
+    await bot.set_my_commands(commands=settings.BOT_COMMAND)
+    await bot.delete_webhook(drop_pending_updates=True)
 
     dp.startup.register(on_statup)
 
