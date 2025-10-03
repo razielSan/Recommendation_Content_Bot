d7ef19399010b66278838ae1307c70c498d7edc4
raziel <tvindzon@gmail.com>
2025-10-03 12:04:35 +0700

    update config/settings.py ( create properties BOT_COMMAND)

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,10 +1,17 @@
+from typing import List
+
+from aiogram.types import BotCommand
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class AppSettings(BaseSettings):
-    """Настройка приложения."""
+    """Настройки приложения."""
 
     TOKEN: str
+    BOT_COMMAND: List[BotCommand] = [
+        BotCommand(command="start", description="Меню бота")
+    ]
 
     model_config: SettingsConfigDict = SettingsConfigDict(env_file=".env")
 
