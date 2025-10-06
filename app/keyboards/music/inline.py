5e4e37e7edc1dc3301aaa645692a40fa29f1da18
raziel <tvindzon@gmail.com>
2025-10-06 14:29:26 +0700

    create functions get_main_musical_news_button(app\keyboards\music\inline.py)

--- app/keyboards/music/inline.py
+++ app/keyboards/music/inline.py
@@ -1,7 +1,7 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.types.inline_keyboard_button import InlineKeyboardButton
 
-from config.music import music_settings
+from config.music import music_settings, discogs_setting
 
 
 def get_music_main_button():
@@ -16,6 +16,25 @@ def get_music_main_button():
                 InlineKeyboardButton(
                     text=music_settings.MUSIC_NEWS,
                     callback_data=music_settings.CALLBACK_MUSIC_NEWS,
+                )
+            ],
+        ]
+    )
+    return inline_kb.as_markup(resize_keyboard=True)
+
+
+def get_main_musical_news_button():
+    """Возвращает инлайн кнопки меню музыкальных новинок
+
+    Returns:
+        _type_: inline_kb
+    """
+    inline_kb = InlineKeyboardBuilder(
+        markup=[
+            [
+                InlineKeyboardButton(
+                    text=discogs_setting.DISCOGS,
+                    callback_data=discogs_setting.CALLBACK_DISCOGS,
                 )
             ],
         ]
