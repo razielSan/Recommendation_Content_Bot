9d70a02f78aeb436a0dd028f71005e27e8d7a357
raziel <tvindzon@gmail.com>
2025-10-06 14:31:04 +0700

    update functions main(app\views\music\musical_news\main.py)

--- app/views/music/musical_news/main.py
+++ app/views/music/musical_news/main.py
@@ -3,7 +3,7 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.filters import StateFilter
 
 from config.music import music_settings
-from keyboards.music.inline import get_music_main_button
+from keyboards.music.inline import get_music_main_button, get_main_musical_news_button
 
 
 router = Router(name=__name__)
@@ -11,4 +11,7 @@ router = Router(name=__name__)
 
 @router.callback_query(StateFilter(None), F.data == music_settings.CALLBACK_MUSIC_NEWS)
 async def main(call: CallbackQuery):
-    await call.message_answer("ok")
+    await call.message.answer(
+        text="Доступные варианты",
+        reply_markup=get_main_musical_news_button(),
+    )
