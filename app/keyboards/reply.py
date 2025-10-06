29021bb1608c675a2a060a6bbd7e0cc6ca8475a2
raziel <tvindzon@gmail.com>
2025-10-06 13:51:51 +0700

    create app\keyboards\reply.py

---
+++ app/keyboards/reply.py
@@ -0,0 +1,20 @@
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.types.keyboard_button import KeyboardButton
+
+from config.music import music_settings
+
+
+def get_main_menu_button():
+    """Возвращает клавиатуру с кнопками главного меню
+
+    Returns:
+        _type_: reply_keyboard
+    """
+    reply_kb = ReplyKeyboardBuilder(
+        markup=[
+            [
+                KeyboardButton(text=music_settings.MUSIC),
+            ],
+        ],
+    )
+    return reply_kb.as_markup(resize_keyboard=True)
