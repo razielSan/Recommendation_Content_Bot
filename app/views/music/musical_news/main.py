b85ad213dea1e1fdf8325b135c3a60b7e5a5cced
raziel <tvindzon@gmail.com>
2025-10-06 13:53:38 +0700

    create app\views\music\musical_news\main.py

---
+++ app/views/music/musical_news/main.py
@@ -0,0 +1,14 @@
+from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery
+from aiogram.filters import StateFilter
+
+from config.music import music_settings
+from keyboards.music.inline import get_music_main_button
+
+
+router = Router(name=__name__)
+
+
+@router.callback_query(StateFilter(None), F.data == music_settings.CALLBACK_MUSIC_NEWS)
+async def main(call: CallbackQuery):
+    await call.message_answer("ok")
