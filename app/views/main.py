92b61aa1a77acf6f9919a2feb0c978ec14fa5dcf
raziel <tvindzon@gmail.com>
2025-10-08 19:51:32 +0700

    update app\views\main.py

--- app/views/main.py
+++ app/views/main.py
@@ -11,7 +11,7 @@ router = Router(name=__name__)
 @router.message(StateFilter(None), F.text == "/start")
 async def main(message: Message):
     """Отправляет пользователю клавиатура с вариантами выбора рекомендаций."""
-    print("ok")
+
     await message.answer(
         text="Доступные варианты",
         reply_markup=get_main_menu_button(),
